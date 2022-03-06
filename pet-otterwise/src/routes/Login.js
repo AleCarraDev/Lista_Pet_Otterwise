4c69fe9d2cc7bb553a6c77b829aaa057732fb318
AleCarraDev <22fimdetarde@gmail.com>
2022-03-06 20:33:17 -0300

    mudando rotas

--- pet-otterwise/src/routes/Login.js
+++ pet-otterwise/src/routes/Login.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import { useAuth } from '../context/auth-context'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useAuth } from "../context/auth-context";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export default function Login() {
-  const auth = useAuth()
-  const location = useLocation()
-  const navigate = useNavigate()
+function Login() {
+  const auth = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const from = location.state?.from?.pathname || '/'
+  const from = location.state?.from?.pathname || "/";
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const email = formData.get('email')
-    const password = formData.get('password')
-
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
     const data = {
       email,
       password,
-    }
-    auth.login(data, () => navigate(from, { replace: true }))
-  }
+    };
+
+    auth.login(data, () => {
+      navigate(from, { replace: true });
+    });
+  };
 
   return (
     <div>
-      Login
       <form onSubmit={handleSubmit}>
         <label>
-          E-mail: <input type="email" name="email" />
+          email: <input type="email" name="email" />
         </label>
         <label>
-          Senha: <input type="password" name="password" />
+          senha: <input type="password" name="password" />
         </label>
-        <button type="submit">Login</button>
+        <button type="submit">Enviar</button>
       </form>
     </div>
-  )
+  );
 }
+
+export default Login;
